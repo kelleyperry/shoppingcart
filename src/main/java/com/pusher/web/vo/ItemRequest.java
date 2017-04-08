ab0c21faede5559ebff9a228a44f050d3174fcbe
kelleyperry <kelleyperry@gmail.com>
2017-04-08 16:36:23 -0700

    optimize file

--- src/main/java/com/pusher/web/vo/ItemRequest.java
+++ src/main/java/com/pusher/web/vo/ItemRequest.java
@@ -12,19 +12,11 @@ public class ItemRequest implements Serializable {
 
     private Integer quantity;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public void setId(Long id) { this.id = id; }
 
-    public Integer getQuantity() {
-        return quantity;
-    }
+    public Integer getQuantity() { return quantity; }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
 }
